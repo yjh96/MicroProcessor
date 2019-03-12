b67243854a670e52f2bddf0db531ee63f89da2ac
yjh96 <11yjh11@naver.com>
2019-03-12 12:37:12 +0900

    Update LED1.c

--- LED1.c
+++ LED1.c
@@ -8,26 +8,20 @@
 #include <mega128.h>
 #include <delay.h>
 #define LED1 PORTA.0
-
+#define ROUND 10
+int n;
 void main(void)
 {
-    int tmp = 0;
+    PORTA = 0x00; // LED 꺼짐
     DDRA = 0xff;
     while (1)
     {
-        PORTA = 0x02;                  //LED 꺼짐
-        delay_ms(100);
-        tmp = tmp + 1;
-        PORTA = 0xff;                  //LED 켜짐
-        delay_ms(100);
-        
-        if ( tmp == 10 ){
-            PORTA = 0x00;
-            delay_ms(1000);
-            PORTA = 0xff;
-            delay_ms(1000);
-            tmp = 0;
-            }
-         
+      for ( n=0; n<ROUND ; n++){
+      PORTA =  PORTA | 0x01; 
+      delay_ms (1000);
+      PORTA = 0x00;
+      delay_ms(1000);
+    }               
+    PORTA = ~PORTA;
     }
 }
